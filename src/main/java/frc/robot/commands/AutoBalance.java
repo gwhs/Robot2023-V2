ad91ffa458bf4284b828fd963def14e187358f2c
Alannables <alanli1032005@gmail.com>
2023-01-14 17:01:07 -0800

    added get pitch, roll, yaw; started auto balance

---
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class AutoBalance extends CommandBase {
+  private final DrivetrainSubsystem drivetrainSubsystem;
+
+  private double currentAngle;
+  private double velocity;
+  private double velocityConstant;
+  private double error;
+
+  /** Creates a new AutoBalance. */
+  public AutoBalance(DrivetrainSubsystem drivetrainSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrainSubsystem = drivetrainSubsystem;
+    velocityConstant = 2;
+
+    addRequirements(drivetrainSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    currentAngle = drivetrainSubsystem.getRoll();
+
+    velocity = currentAngle * velocityConstant;
+
+    drivetrainSubsystem.drive(new ChassisSpeeds(velocity, 0.0, 0.0));
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
