ac73d20746c8c9dd6d14810e818275beb46cb522
bennables <bensiriv@gmail.com>
2023-01-16 15:54:17 -0800

    read documentation wrong, added things to set, previous was things to read

--- src/main/java/frc/robot/subsystems/LimeVision/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeVision/LimeLight.java
@@ -20,7 +20,7 @@ public class LimeLight{
 	}
 
   /*
-  These are possible entries
+  These are possible entries to ***GET***
   tv 	Whether the limelight has any valid targets (0 or 1)
   tx 	Horizontal Offset From Crosshair To Target (LL1: -27 degrees to 27 degrees | LL2: -29.8 to 29.8 degrees)
   ty 	Vertical Offset From Crosshair To Target (LL1: -20.5 degrees to 20.5 degrees | LL2: -24.85 to 24.85 degrees)
@@ -69,6 +69,39 @@ public class LimeLight{
 			.getEntry(entry_name)
 			.getNumberArray(new Number[0]);
 	}
+
+
+  /*
+   * Possible things to set:
+   * ledMode 	Sets limelight’s LED state
+    0 	use the LED Mode set in the current pipeline
+    1 	force off
+    2 	force blink
+    3 	force on
+
+    camMode 	Sets limelight’s operation mode
+    0 	Vision processor
+    1 	Driver Camera (Increases exposure, disables vision processing)
+
+    pipeline 	Sets limelight’s current pipeline
+    0 .. 9 	Select pipeline 0..9
+
+    stream 	Sets limelight’s streaming mode
+    0 	Standard - Side-by-side streams if a webcam is attached to Limelight
+    1 	PiP Main - The secondary camera stream is placed in the lower-right corner of the primary camera stream
+    2 	PiP Secondary - The primary camera stream is placed in the lower-right corner of the secondary camera stream
+
+    snapshot 	Allows users to take snapshots during a match
+    0 	Reset snapshot mode
+    1 	Take exactly one snapshot
+
+    crop 	Sets the crop rectangle. The pipeline must utilize the default crop rectangle in the web interface. The array must have exactly 4 entries.
+    [0] 	X0 - Min or Max X value of crop rectangle (-1 to 1)
+    [1] 	X1 - Min or Max X value of crop rectangle (-1 to 1)
+    [2] 	Y0 - Min or Max Y value of crop rectangle (-1 to 1)
+    [3] 	Y1 - Min or Max Y value of crop rectangle (-1 to 1)
+   * 
+   */
 
 	public void set_entry_double(String entry_name, double value) {
 		NetworkTableInstance.getDefault()
