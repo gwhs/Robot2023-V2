2860d41d2b0175949cadd2db9d4cd106f31c67e8
Alannables <alanli1032005@gmail.com>
2023-01-16 12:05:47 -0800

    timer

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
@@ -12,41 +13,68 @@ public class AutoBalance extends CommandBase {
   private final DrivetrainSubsystem drivetrainSubsystem;
 
   private double currentAngle;
-  private double velocity;
-  private double velocityConstant;
-  private double error;
+  private double speed;
+  private double speedConstant;
+  private Timer timer;
+  private final double DESIRED_ENGAGE_TIME; //in milliseconds
 
   /** Creates a new AutoBalance. */
   public AutoBalance(DrivetrainSubsystem drivetrainSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.drivetrainSubsystem = drivetrainSubsystem;
-    velocityConstant = 2;
+    speedConstant = 0.2;
+    timer = new Timer();
+    timer.stop();
+    DESIRED_ENGAGE_TIME = 1000;
 
     addRequirements(drivetrainSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     currentAngle = drivetrainSubsystem.getRoll();
 
-    velocity = currentAngle * velocityConstant;
+    speed = currentAngle * speedConstant;
 
-    drivetrainSubsystem.drive(new ChassisSpeeds(velocity, 0.0, 0.0));
+    if (speed > 0.2)
+    {
+      speed = 0.2;
+    }
+    else
+    {
+
+    }
+
+    if (Math.abs(currentAngle) <= 2.5)
+    {
+      timer.start();
+    }
+    else
+    {
+      timer.stop();
+      timer.reset();
+    }
+
+    drivetrainSubsystem.drive(new ChassisSpeeds(speed, 0.0, 0.0));
 
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drivetrainSubsystem.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return timer.get() >= DESIRED_ENGAGE_TIME;
   }
 }
