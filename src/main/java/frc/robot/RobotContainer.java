e90e68dddbf20a4e74b6bd10fba37f37ee726e1b
Chvoong24 <89755355+Chvoong24@users.noreply.github.com>
2023-01-18 16:00:56 -0800

    made a 1 m straigth path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,7 +148,7 @@ public class RobotContainer {
     // controller.x().
     //     whileTrue(new PPAStar(drivetrainSubsystem, poseEstimator, 
     //         new PathConstraints(2, 2), finalNode, obstacles, AStarMap));
-    controller.y().toggleOnTrue(new TestAutonomous(drivetrainSubsystem, poseEstimator));
+    controller.y().onTrue(new TestAutonomous(drivetrainSubsystem, poseEstimator));
   }
 
   /**
