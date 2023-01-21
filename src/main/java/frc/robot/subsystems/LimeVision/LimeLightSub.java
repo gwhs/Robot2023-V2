c71628aef961ee427e916e88c28efe91a47452f4
bennables <bensiriv@gmail.com>
2023-01-21 12:38:35 -0800

    a little bit of netowkrtalbe things, ned more work

--- src/main/java/frc/robot/subsystems/LimeVision/LimeLightSub.java
+++ src/main/java/frc/robot/subsystems/LimeVision/LimeLightSub.java
@@ -4,45 +4,51 @@
 
 package frc.robot.subsystems.LimeVision;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimeLightSub extends SubsystemBase {
 
-   // set up a new instance of NetworkTables (the api/library used to read values from limelight)
-   NetworkTable networkTable = NetworkTableInstance.getDefault().getTable("limelight");
-   
-  
-   // return network table values for tx and ty using getEntry()
-   NetworkTableEntry tv = networkTable.getEntry("tv"); // Whether the limelight has any valid targets (0 or 1)
-   NetworkTableEntry tx = networkTable.getEntry("tx"); // Horizontal Offset From Crosshair To Target (LL1: -27 degrees to 27 degrees | LL2: -29.8 to 29.8 degrees)
-   NetworkTableEntry ty = networkTable.getEntry("ty"); // Vertical Offset From Crosshair To Target (LL1: -20.5 degrees to 20.5 degrees | LL2: -24.85 to 24.85 degrees)
-   NetworkTableEntry ta = networkTable.getEntry("ta"); // Target Area (0% of image to 100% of image)
-   NetworkTableEntry ts = networkTable.getEntry("ts"); // Skew or rotation (-90 degrees to 0 degrees)
- 
+  // set up a new instance of NetworkTables (the api/library used to read values from limelight)
+  NetworkTable networkTable = NetworkTableInstance.getDefault().getTable("limelight");
 
-   private double kCameraHeight = 5;//LimelightConstants.kCameraHeight;
-   private double kTargetHeight = 5;//LimelightConstants.kTargetHeight;
- 
-   private double theta;
-   private double distance;
+  // return network table values for tx and ty using getEntry()
+  NetworkTableEntry tv =
+      networkTable.getEntry("tv"); // Whether the limelight has any valid targets (0 or 1)
+  NetworkTableEntry tx =
+      networkTable.getEntry(
+          "tx"); // Horizontal Offset From Crosshair To Target (LL1: -27 degrees to 27 degrees |
+  // LL2: -29.8 to 29.8 degrees)
+  NetworkTableEntry ty =
+      networkTable.getEntry(
+          "ty"); // Vertical Offset From Crosshair To Target (LL1: -20.5 degrees to 20.5 degrees |
+  // LL2: -24.85 to 24.85 degrees)
+  NetworkTableEntry ta = networkTable.getEntry("ta"); // Target Area (0% of image to 100% of image)
+  NetworkTableEntry ts = networkTable.getEntry("ts"); // Skew or rotation (-90 degrees to 0 degrees)
 
-   private LimeLightComms limelight_comm;
-   private String limeLightName;
+  private double kCameraHeight = 5; // LimelightConstants.kCameraHeight;
+  private double kTargetHeight = 5; // LimelightConstants.kTargetHeight;
+
+  private double theta;
+  private double distance;
+
+  private LimeLightComms limelight_comm;
+  private String limeLightName;
 
   /** Creates a new LimeLightSub. */
-
-	public LimeLightSub(String limeLightName, String limelight_networktable_name) {
-		limelight_comm = new LimeLightComms(limelight_networktable_name);
+  public LimeLightSub(String limeLightName, String limelight_networktable_name) {
+    limelight_comm = new LimeLightComms(limelight_networktable_name);
     this.limeLightName = limeLightName;
-	}
+  }
 
   @Override
   public void periodic() {
-    distance = (kTargetHeight-kCameraHeight)/(Math.tan(Math.toRadians(getTy() + 5))//LimelightConstants.mountingAngle))
-    *(Math.cos(Math.toRadians(getTx()))));
+    distance =
+        (kTargetHeight - kCameraHeight)
+            / (Math.tan(Math.toRadians(getTy() + 5)) // LimelightConstants.mountingAngle))
+                * (Math.cos(Math.toRadians(getTx()))));
     SmartDashboard.putNumber("tv", tv.getDouble(0));
     SmartDashboard.putNumber("tx", tx.getDouble(0));
     SmartDashboard.putNumber("ty", ty.getDouble(0));
@@ -59,6 +65,12 @@ public class LimeLightSub extends SubsystemBase {
   public double getTy() {
     return ty.getDouble(0);
   }
-  public double getTheta() {return theta;}
-  public double getDistance() {return distance;}
+
+  public double getTheta() {
+    return theta;
+  }
+
+  public double getDistance() {
+    return distance;
+  }
 }
