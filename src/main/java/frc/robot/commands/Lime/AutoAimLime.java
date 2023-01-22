9e3dadd3e88851b83c9f99aefb407b4932a170c2
bennables <bensiriv@gmail.com>
2023-01-21 16:46:11 -0800

    testing code and code for pos and neg x locations

--- src/main/java/frc/robot/commands/Lime/AutoAimLime.java
+++ src/main/java/frc/robot/commands/Lime/AutoAimLime.java
@@ -5,6 +5,7 @@
 package frc.robot.commands.Lime;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.LimeLightConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -13,7 +14,7 @@ import frc.robot.subsystems.LimeVision.LimeLightSub;
 public class AutoAimLime extends CommandBase {
   private DrivetrainSubsystem drivetrainSubsystem;
   private LimeLightSub limeLight;
-  private boolean done = false;
+  private boolean pos;
   private double targetX = 1;
   private double targetY = LimeLightConstants.MAX_LIMELIGHT_ERROR_DEGREES;
 
@@ -28,39 +29,35 @@ public class AutoAimLime extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    done = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // if (limeLight.getTx() < 0) {
-    //   while (limeLight.getTx() < targetX) {
-    //     drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, Math.toRadians(10)));
-    //   }
-    // } else {
-    //   while (Math.abs(limeLight.getTx()) > targetX) {
-    //     drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, Math.toRadians(-10)));
-    //   }
-    // }
-    // drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, Math.toRadians(0)));
-    // done = true;
-    if (limeLight.getTx() < 0) {
-      drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, Math.toRadians(10)));
+    if (limeLight.getTx() >= 0){
+      pos = true;
+      drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, Math.toRadians(-20)));
+    } else{
+      pos = false;
+      drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, Math.toRadians(20)));
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    SmartDashboard.putNumber("Final X-error" ,limeLight.getTx());
+    SmartDashboard.putNumber("Final Y-error", limeLight.getTy());
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (limeLight.getTx() > 0) {
+    if ((pos && limeLight.getTx() < 0) || (!pos && limeLight.getTx() > 0)) {
       drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, Math.toRadians(0)));
       return true;
     }
+    
     return false;
   }
 }
