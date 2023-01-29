c41193835cf990ae80267054ab18524ec5c6d73c
bennables <bensiriv@gmail.com>
2023-01-28 17:46:22 -0800

    pid is in, but the finishing condition is broken

--- src/main/java/frc/robot/commands/Lime/PPIDAutoAim.java
+++ src/main/java/frc/robot/commands/Lime/PPIDAutoAim.java
@@ -7,6 +7,12 @@ package frc.robot.commands.Lime;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DrivetrainConstants;
@@ -21,6 +27,9 @@ public class PPIDAutoAim extends CommandBase {
   private boolean horizDone = false;
   private boolean angleDone = false;
   private double angleGoal = 0;
+  private final GenericEntry pentry;
+  private final GenericEntry dentry;
+  private final GenericEntry ientry;
   private double initAngle;
   private double targetY = LimeLightConstants.MAX_LIMELIGHT_ERROR_DEGREES;
   // second param on constraints is estimated, should be max accel, not max speed, but lets say it
@@ -29,15 +38,25 @@ public class PPIDAutoAim extends CommandBase {
       new Constraints(
           DrivetrainConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
           DrivetrainConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND);
-  private ProfiledPIDController pid = new ProfiledPIDController(3, 0, .01, constraints);
+  private double p = 3;
+  private double i = 0;
+  private double d = .1;
+  private final ShuffleboardTab tab;
+  private ProfiledPIDController pid = new ProfiledPIDController(p, i, d, constraints);
 
   /** Creates a new AutoAimLime. */
   public PPIDAutoAim(DrivetrainSubsystem drivetrainSubsystem, LimeLightSub limeLightSub) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.limeLight = limeLightSub;
     this.drivetrainSubsystem = drivetrainSubsystem;
-    pid.setTolerance(Math.toRadians(1));
+
     addRequirements(drivetrainSubsystem);
+    tab = Shuffleboard.getTab("limepid");
+    ShuffleboardLayout input =
+        tab.getLayout("Constant Inputs", BuiltInLayouts.kList).withSize(2, 4).withPosition(2, 0);
+    pentry = input.add("p Constant", p).withWidget(BuiltInWidgets.kTextView).getEntry();
+    dentry = input.add("d Constant", d).withWidget(BuiltInWidgets.kTextView).getEntry();
+    ientry = input.add("i constant", i).withWidget(BuiltInWidgets.kTextView).getEntry();
   }
 
   // Called when the command is initially scheduled.
@@ -45,10 +64,15 @@ public class PPIDAutoAim extends CommandBase {
   public void initialize() {
     horizDone = false;
     angleDone = false;
-    initAngle = limeLight.hasTarget() ? limeLight.getTx() : 0;
 
+    initAngle = limeLight.hasTarget() ? limeLight.getTx() : 0;
+    pid.reset(Math.toRadians(initAngle));
+    p = pentry.getDouble(p);
+    d = dentry.getDouble(d);
+    i = ientry.getDouble(i);
     // goal velocity is 0 (overloaded constructor)
     pid.setGoal(Math.toRadians(angleGoal));
+    pid.setTolerance(Math.toRadians(10));
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -67,6 +91,9 @@ public class PPIDAutoAim extends CommandBase {
     } else {
       horizDone = false;
     }
+    System.out.printf("X equals %f PID moves %f%n", limeLight.getTx(), values[2]);
+    System.out.println();
+    //atgoal is not working
     if (pid.atGoal()) {
       angleDone = true;
     } else {
@@ -86,7 +113,7 @@ public class PPIDAutoAim extends CommandBase {
   @Override
   public boolean isFinished() {
     // && angleDone
-    return angleDone && horizDone;
+    return angleDone;
   }
 
   public double[] chassisValuesLower() {
