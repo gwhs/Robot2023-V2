8e6708ac24e8502618aaf6b30bc8fa33451239ab
bennables <bensiriv@gmail.com>
2023-02-04 15:09:01 -0800

    rotate should be a different function

---
+++ src/main/java/frc/robot/commands/Lime/Rotate.java
@@ -0,0 +1,128 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Lime;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.LimeLightConstants;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.LimeVision.LimeLightSub;
+import frc.robot.subsystems.PoseEstimatorSubsystem;
+
+public class Rotate extends CommandBase {
+  private DrivetrainSubsystem drivetrainSubsystem;
+  private PoseEstimatorSubsystem poseEstimatorSubsystem;
+  private LimeLightSub limeLight;
+  private double[] values = {0, 0, 0};
+  private boolean sidewaysDone = false;
+  private boolean angleDone = false;
+  // private final GenericEntry pentry;
+  // private final GenericEntry dentry;
+  // private final GenericEntry ientry;
+  private double sideWaysError;
+  // second param on constraints is estimated, should be max accel, not max speed, but lets say it
+  // gets there in a second
+  private Constraints angleConstraints =
+      new Constraints(
+          DrivetrainConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+          DrivetrainConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND);
+
+  //// second param on constraints is estimated, should be max accel, not max speed, but lets say it
+  // gets there in a second
+  private Constraints positionConstraints =
+      new Constraints(
+          DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND / 5,
+          DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND / 5);
+
+  private double angleP = 5;
+  private double angleI = 0;
+  private double angleD = 0;
+  // private final ShuffleboardTab tab;
+  private ProfiledPIDController anglePid =
+      new ProfiledPIDController(angleP, angleI, angleD, angleConstraints);
+
+  private double sidewaysP = .5;
+  private double sidewaysI = 0;
+  private double sidewaysD = 0;
+  private ProfiledPIDController sidewaysPid =
+      new ProfiledPIDController(sidewaysP, sidewaysI, sidewaysD, positionConstraints);
+
+  /** Creates a new AutoAimLime. */
+  public Rotate(
+      DrivetrainSubsystem drivetrainSubsystem,
+      PoseEstimatorSubsystem poseEstimatorSubsystem,
+      LimeLightSub limeLightSub) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.limeLight = limeLightSub;
+    this.poseEstimatorSubsystem = poseEstimatorSubsystem;
+    this.drivetrainSubsystem = drivetrainSubsystem;
+
+    addRequirements(drivetrainSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    angleDone = false;
+    sidewaysDone = false;
+
+    // rotating to align
+    anglePid.reset(Math.toRadians(poseEstimatorSubsystem.getAngle()));
+    anglePid.setGoal(Math.toRadians(0));
+    anglePid.setTolerance(Math.toRadians(.5));
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    // add pids
+    values = chassisValuesLower();
+    drivetrainSubsystem.drive(new ChassisSpeeds(values[0], values[1], values[2]));
+    System.out.printf(
+        "X equals %.2f PID moves %.2f%n", poseEstimatorSubsystem.getAngle(), values[2]);
+    // atgoal is not working, it needs it to be == setpoint and be in setpoint.
+    // setpoint just makes sure it's in the tolerance, doesn't work
+    if (Math.abs(poseEstimatorSubsystem.getAngle()) < .5) {
+      angleDone = true;
+    } else {
+      angleDone = false;
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    SmartDashboard.putNumber("Final Horiz-error", limeLight.getTx());
+    SmartDashboard.putNumber("Final Vert-error", limeLight.getTy());
+    drivetrainSubsystem.drive(new ChassisSpeeds(0, 0, 0));
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return angleDone;
+  }
+
+  public double[] chassisValuesLower() {
+    /*
+    [1,2,3]
+    1 is x velocity
+    2 is y velocity
+    3 is degrees rotation
+    */
+
+    // motor.set(controller.calculate(encoder.getDistance(), goal));
+    double[] x = new double[3];
+    x[0] = 0;
+    // calculate is overloaded, second parameter is angle goal if it changes
+    x[2] = angleDone ? 0 : (anglePid.calculate(Math.toRadians(poseEstimatorSubsystem.getAngle())));
+    return x;
+  }
+}
