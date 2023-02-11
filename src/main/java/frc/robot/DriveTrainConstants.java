af9e20271ada70d1868d0364a92985ac7a7ae2f5
samLiu14 <sammiliu71@gmail.com>
2023-02-11 11:27:44 -0800

    offsets

--- src/main/java/frc/robot/DriveTrainConstants.java
+++ src/main/java/frc/robot/DriveTrainConstants.java
@@ -32,8 +32,7 @@ public class DriveTrainConstants {
 
   public static DriveTrainConstants calliope = new DriveTrainConstants(284.4, 304.1, 314.2, 284.5);
 
-  public static DriveTrainConstants chris =
-      new DriveTrainConstants(285.661, 241.828, 297.234, 40.836);
+  public static DriveTrainConstants chris = new DriveTrainConstants(8.846, 0, 0, 0);
 
   public DriveTrainConstants(
       double frontLeftOffset,
--- src/main/java/frc/robot/swerve/ModuleConfiguration.java
+++ src/main/java/frc/robot/swerve/ModuleConfiguration.java
@@ -58,7 +58,7 @@ public enum ModuleConfiguration {
       (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 45.0),
       false,
       (15.0 / 32.0) * (10.0 / 60.0),
-      false);
+      true);
 
   private final double wheelDiameter;
   private final double driveReduction;
