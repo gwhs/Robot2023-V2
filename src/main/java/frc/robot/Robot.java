6258f0ea3e8b53154d46899c6ac07211dc74e7e5
bennables <bensiriv@gmail.com>
2023-02-13 19:37:39 -0800

    may have some problems with the autocommand

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,7 +30,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
+    // autonomous chooser on the dasfhboard.
 
     switch (container) {
       case CHRIS:
