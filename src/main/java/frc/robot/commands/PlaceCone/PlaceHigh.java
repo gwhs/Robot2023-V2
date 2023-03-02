2ccac78dd4d0a0a6fac42a9053d664b0b06b5e20
Alannables <alanli1032005@gmail.com>
2023-03-01 17:03:56 -0800

    good for low

--- src/main/java/frc/robot/commands/PlaceCone/PlaceHigh.java
+++ src/main/java/frc/robot/commands/PlaceCone/PlaceHigh.java
@@ -5,15 +5,34 @@
 package frc.robot.commands.PlaceCone;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.PoseEstimatorSubsystem;
+import frc.robot.subsystems.LimeVision.LimeLightSub;
+import frc.robot.Constants.LimeLightConstants;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class PlaceHigh extends SequentialCommandGroup {
   /** Creates a new PlaceHigh. */
-  public PlaceHigh() {
+  public PlaceHigh(DrivetrainSubsystem drivetrainSubsystem, PoseEstimatorSubsystem poseEstimatorSubsystem, LimeLightSub limeLightSub) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(new PPIDAutoAim(
+            drivetrainSubsystem,
+            poseEstimatorSubsystem,
+            limeLightSub,
+            LimeLightConstants.LOWER_DISTANCE_SHOOT),
+        new WaitCommand(.5),
+        new Rotate(drivetrainSubsystem, poseEstimatorSubsystem, limeLightSub, 0),
+        new StraightWheel(drivetrainSubsystem),
+        new Sideways(drivetrainSubsystem, poseEstimatorSubsystem, limeLightSub),
+        new StraightWheel(drivetrainSubsystem),
+        new PPIDAutoAim(
+            drivetrainSubsystem,
+            poseEstimatorSubsystem,
+            limeLightSub,
+            LimeLightConstants.UPPER_DISTANCE_SHOOT));
   }
 }
