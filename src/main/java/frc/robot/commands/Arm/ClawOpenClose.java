8ccef720c85ebfcb29bd9edacb5657882a83b019
Alannables <alanli1032005@gmail.com>
2023-03-06 19:24:19 -0800

    sb claw

--- src/main/java/frc/robot/commands/Arm/ClawOpenClose.java
+++ src/main/java/frc/robot/commands/Arm/ClawOpenClose.java
@@ -4,25 +4,70 @@
 
 package frc.robot.commands.Arm;
 
+import java.util.List;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import frc.robot.subsystems.ArmSubsystems.Claw;
 
 public class ClawOpenClose extends CommandBase {
 
   private Claw claw;
+
+  private final ShuffleboardTab tab;
+
   private double angle;
+  private final GenericEntry desiredAngleEntry;
+  private double desiredAngleDefault;
+
   private int amps;
+  private final GenericEntry ampsEntry;
+  private double ampsDefault;
 
   public ClawOpenClose(double angle, int amps, Claw claw) {
     this.claw = claw;
-    this.angle = angle;
-    this.amps = amps;
+    this.desiredAngleDefault = angle;
+    this.ampsDefault = amps;
+
+    tab = Shuffleboard.getTab("Arm");
+
+    ShuffleboardLayout openCloseShit =
+        tab.getLayout("Claw Open Close Shit", BuiltInLayouts.kList)
+            .withSize(2, 4)
+            .withPosition(4, 0);
+
+    if (openCloseShit.getComponents().isEmpty()) {
+      desiredAngleEntry =
+          openCloseShit
+              .add("Desired Angle", desiredAngleDefault)
+              .withWidget(BuiltInWidgets.kTextView)
+              .getEntry();
+      ampsEntry =
+          openCloseShit
+              .add("Amps", ampsDefault)
+              .withWidget(BuiltInWidgets.kTextView)
+              .getEntry();
+    } else {
+      List<ShuffleboardComponent<?>> widgets = openCloseShit.getComponents();
+      desiredAngleEntry = ((SimpleWidget) widgets.get(0)).getEntry();
+      ampsEntry = ((SimpleWidget) widgets.get(1)).getEntry();
+    }
+
     addRequirements(claw);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    angle = desiredAngleEntry.getDouble(desiredAngleDefault);
+    amps = (int) ampsEntry.getDouble(ampsDefault);
     claw.setLimit(amps);
     claw.setPosition(angle);
   }
