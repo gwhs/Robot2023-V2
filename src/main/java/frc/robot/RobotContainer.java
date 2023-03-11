4861bdc5cd9dc39aac93e99268abcd6df15002a9
bennables <bensiriv@gmail.com>
2023-03-11 08:31:35 -0800

    merged with path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -632,60 +632,7 @@ public class RobotContainer {
     controllertwo.back().onTrue(Commands.runOnce(poseEstimator::set180FieldPosition, drivetrainSubsystem));
  
  
- 
 
- 
-     // controller.y().onTrue(straightWheel1);
-     controllertwo
-         // Place high //5 , 2.5, 5
-         .y()
-         .onTrue(
-             Commands.sequence(
-                 Commands.print("START"),
-                 // new ClawEncoderMoveDown(-100, clawPivot, clawEncoder, "Cube").withTimeout(1.5),
-                 // new PPIDAutoAim(drivetrainSubsystem, limeLightSub, 44),
-                 // Commands.waitSeconds(.25),
-                 // new MagicMotionPos(mainArm, 40, 1, 1, 5),
-                 new MagicMotionPosShuffleboard(mainArm, 190, 2.75, 5),
-                 // Commands.waitSeconds(.1),
-                 // new MagicMotionPosShuffleboard(mainArm, 180, 1, 1),
-                 // Commands.waitSeconds(),
-                 new MagicMotionPos(mainArm, 30, 3, 1.5, .5),
-                 Commands.waitSeconds(.5),
-                 // new ClawEncoderMoveUp(0, clawPivot, clawEncoder, "Cube"),
-                 // Commands.waitSeconds(.3),
- 
-                 // new MagicMotionPosShuffleboard(mainArm, 190, 2.75, 5),
-                 // new MagicMotionPos(mainArm, 15, 3, 1.5, .5),
-                 // Commands.waitSeconds(.5),
-                 new MagicMotionAbsoluteZero(mainArm, shaftEncoder, 5, 2.5)));
-     // CUBE
-     controllertwo
-         .rightBumper()
-         .onTrue(
-             Commands.either(
-                 new ClawEncoderMoveDown(-125, clawPivot, clawEncoder, "Cube").withTimeout(1.5),
-                 Commands.sequence(
-                     Commands.print("Encoder Pos" + -clawEncoder.getRaw() / 8192. * 360.),
-                     Commands.parallel(
-                         new ClawOpenCloseShuffleBoard(25, 5, clawOpenClose),
-                         Commands.waitSeconds(1)),
-                     new ClawEncoderMoveUp(0, clawPivot, clawEncoder, "CUBE"),
-                     new ClawOpenClose(0, 5, clawOpenClose).withTimeout(2)),
-                 clawEncoder::posDown));
- 
-     // CONE
-     // controllertwo
-     //     .leftTrigger()
-     //     .onTrue(
-     //         Commands.either(
-     //             new ClawEncoderMoveDown(-125, clawPivot, clawEncoder, "CONE").withTimeout(3),
-     //             Commands.sequence(
-     //                 Commands.parallel(
-     //                     new ClawOpenClose(-85, 20, clawOpenClose), Commands.waitSeconds(1)),
-     //                 new ClawEncoderMoveUp(0, clawPivot, clawEncoder, "CONE").withTimeout(3),
-     //                 new ClawOpenClose(0, 20, clawOpenClose)),
-     //             clawEncoder::posDown));
  
   }
 }
