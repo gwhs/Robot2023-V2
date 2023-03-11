85886867c5499a7b290d6c383e6738f324ea127d
samLiu14 <sammiliu71@gmail.com>
2023-03-11 13:07:04 -0800

    Added a command for Movin intake up & down

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -646,6 +646,19 @@ l
     controllertwo
         .back()
         .onTrue(Commands.runOnce(poseEstimator::set180FieldPosition, drivetrainSubsystem));
+
+
+    controllertwo
+    .leftTrigger()
+    .onTrue(
+        Commands.either(
+            new ClawEncoderMoveDown(-125, clawPivot, clawEncoder, "CONE").withTimeout(3),
+            // Commands.sequence(
+                // Commands.parallel(
+                    // new ClawOpenClose(100, 20, clawOpenClose), Commands.waitSeconds(1)),
+            new ClawEncoderMoveUp(0, clawPivot, clawEncoder, "CONE").withTimeout(3),
+                // new ClawOpenClose(20, 20, clawOpenClose)),
+            clawEncoder::posDown));
   }
 
   // zoey
