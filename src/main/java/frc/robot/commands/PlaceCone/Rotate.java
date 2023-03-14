57836d42a4b16edb3928bf9a955b484ee76be806
Bennables <67711255+Bennables@users.noreply.github.com>
2023-03-14 16:22:28 -0700

    Update Rotate.java

--- src/main/java/frc/robot/commands/PlaceCone/Rotate.java
+++ src/main/java/frc/robot/commands/PlaceCone/Rotate.java
@@ -117,7 +117,7 @@ public class Rotate extends CommandBase {
     // System.out.printf(
     //     "X equals %.2f PID moves %.2f%n", poseEstimatorSubsystem.getAngle(), values[2]);
     // setpoint and atgoal don't work, just brute forced.
-    if (MathUtil.inputModulus(poseEstimatorSubsystem.getAngle(), 0,360) < 2) {
+    if (    if (Math.abs(180 - poseEstimatorSubsystem.getAngle()) < 2){
       angleDone = true;
     } else {
       angleDone = false;
